c8b0ede72f5f395672c960607cb65cb24d532b65
Miguel-andres <silva.andresmiguel@gmail.com>
2021-05-03 23:34:14 -0300

    routeo  home y register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './global.css';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import Login from "./components/Login"
 import Footer from './components/Footer';
+import Home from "./components/Home"
+import Error404 from "./components/NotFound"
 
 function App() {
   return (
-    <div className="App ">
-     <Login/>
-     <Footer/>
-    </div>
+    <Router>
+      
+      <div className="App ">
+      
+     <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Login />         
+        </Route>
+
+        <Route path="*">
+          <Error404/>
+        </Route>
+
+      </Switch>
+      <Footer />
+     
+     </div>
+
+
+    </Router>
+
   );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>No existe la pagina</h2>
+        </div>
+    )
+}
